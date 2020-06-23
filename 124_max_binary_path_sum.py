ae1bf6927b2d1fc0a16e12c0f0d4433c2f5f9175
hcxie20 <hcxie20@126.com>
2020-06-23 09:21:56 +0800

    06.23

--- 124_max_binary_path_sum.py
+++ 124_max_binary_path_sum.py
@@ -8,25 +8,37 @@ class Solution:
 
         def max_gain(node):
             nonlocal rst
+            node_path = node.val
+
             if node.left is None and node.right is None:
                 rst = max(rst, node.val)
                 return node.val
 
-            node_gain = node.val
-
-            if node.left:
+            elif node.right is None:
                 left = max_gain(node.left)
                 if left > 0:
-                    node_gain += left
+                    node_path += left
+                rst = max(rst, node_path)
+                return node_path
 
-            if node.right:
+            elif node.left is None:
                 right = max_gain(node.right)
                 if right > 0:
-                    node_gain += right
+                    node_path += right
+                rst = max(rst, node_path)
+                return node_path
             
-            rst = max(rst, node_gain)
-            
-            return node_gain
-
+            else:
+                left = max_gain(node.left)
+                right = max_gain(node.right)
+                for num in [left, right]:
+                    if num > 0:
+                        node_path += num
+                rst = max(rst, node_path)
+                
+                if node_path > node.val:
+                    return node.val + max(left, right)
+                else:
+                    return node.val
         max_gain(root)
         return rst
